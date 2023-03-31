1c92f3ccb2d2baa773d1a17206aed70619458ae4
yasuoOwO <heinmina05@gmail.com>
2023-03-31 21:31:59 +0630

    mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,14 @@ import {AiOutlineSearch} from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import logo from '../../public/tmdb.svg'
 import {BiMenuAltLeft} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [menu,setMenu] = useState(false);
+    const menuHandleClick = () => {
+        if(menu === false) setMenu(true);
+        else setMenu(false);
+    }
   return (
     <div className=' bg-headLineColor py-5'>
         <div className="container mx-auto">
@@ -13,6 +19,7 @@ const Navbar = () => {
                     <Link to={'/'}>
                         <img src={logo} className=' lg:w-52 w-44' alt="" />
                     </Link>
+                    {/* for laptops  */}
                     <div className=" hidden lg:block ">
                         <ul className=' flex text-lg  gap-5 font-medium  text-background'>
                             <Link to={'/'}>
@@ -27,6 +34,7 @@ const Navbar = () => {
                             </Link>
                         </ul>
                     </div>
+                    
                 </div>
                 <div className="hidden lg:block">
                     <div className="">
@@ -40,11 +48,31 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className="hamburger-menu block lg:hidden">
-                    <div className="px-3 py-3 rounded-full  text-background cursor-pointer">
+                    <div onClick={menuHandleClick} className="px-3 py-3 rounded-full  text-background cursor-pointer">
                         <BiMenuAltLeft className=' text-3xl   '/>
                     </div>
                 </div>
+                
             </div>
+            {/* for mobile  */}
+            {
+                menu !== false && (
+                    <div className={`pt-5  transition-all duration-500  lg:hidden`}>
+                        <ul className=' flex flex-col text-lg  gap-5 font-medium  text-background'>
+                            <Link to={'/'}>
+                                <li>Home</li>
+                            </Link>
+                            <Link to={'/movies'}>
+                                <li>Movies</li>
+                            </Link>
+                            <li>TV Shows</li>
+                            <Link to={'/person'}>
+                                <li>People</li>
+                            </Link>
+                        </ul>
+                    </div>
+                )
+            }
         </div>
     </div>
   )
